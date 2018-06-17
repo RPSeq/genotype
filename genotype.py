6f2a3a9c6a8d4c8149dea34d52018a89e7065399
RPSeq <ryan.smith.p@gmail.com>
2018-06-16 17:23:26 -0700

    Full unit tests

--- genotype.py
+++ genotype.py
@@ -8,7 +8,14 @@ but I chose to stick to python builtins.
 import sys
 import argparse
 import unittest
-import StringIO
+
+# StringIO import is different between python2.x and python3.x
+try:
+    # python 2.x: StringIO.StringIO
+    from StringIO import StringIO
+except ImportError:
+    # python 3.x: io.StringIO
+    from io import StringIO
 
 # set script meta vars
 __author__ = "Ryan Smith (ryan.smith.p@gmail.com)"
@@ -23,24 +30,37 @@ def get_args(args):
         description="author: " + __author__ + "\nversion: " + __version__
         )
 
+    # add output argument
+    parser.add_argument(
+        '-t', '--test',
+        action='store_true',
+        required=False,
+        help="Run unit tests"
+        )
+
     # add input argument
     parser.add_argument(
         '-i', '--input',
-        type=argparse.FileType('r'),
-        required=False, default=None,
+        type=str,
+        required=False,
         help="Input file [stdin]"
         )
 
     # add output argument
     parser.add_argument(
         '-o', '--output',
-        type=argparse.FileType('w'),
-        required=False, default=sys.stdout,
+        type=str,
+        required=False,
         help="Output file [stdout]"
         )
 
     # parse the user arguments
     args = parser.parse_args(args)
+
+    # send back the user input
+    return args, parser
+
+def set_inputs(args, parser):
     # if no input arg, check if part of pipe and if so, read stdin.
     if args.input is None:
         if sys.stdin.isatty():
@@ -48,9 +68,17 @@ def get_args(args):
             exit(1)
         else:
             args.input = sys.stdin
-    # send back the user input
-    return args
+    # otherwise, open input by filename
+    else:
+        args.input = open(args.input, 'r')
 
+    # if no output specified, set to stdout
+    if args.output is None:
+        args.output = sys.stdout
+    # otherwise, open output by filename
+    else:
+        args.output = open(args.output, 'w')
+    return
 
 def input_parser(input_file):
     """Generator function. Parses muliplexed genotyping experiments file"""
@@ -67,8 +95,6 @@ def input_parser(input_file):
             yield experiment
             experiment = []
 
-    # we are finished with input
-    input_file.close()
     # make sure to yield final experiment (if no newline at last test)
     if experiment:
         yield experiment
@@ -77,25 +103,27 @@ def input_parser(input_file):
 def filter_mutants(mutant_sample_sets, normal_samples):
     """Filters any mutant from a test if called in normal tests"""
     # exclude normal sample_ids from individual mutant test sets
-    return [test - normal_samples for test in mutant_sample_sets]
+    return [test_set - normal_samples for test_set in mutant_sample_sets]
 
 
 def get_single_mutants(filtered_mutants):
     """Returns a set of sample_ids uniquely identified as mutant"""
     # next(iter(x)) to get the value from a set of len 1
-    #   WITHOUT leaving an empty set behind.
+    #   WITHOUT leaving an empty set behind (with set.pop())
     return set([next(iter(x)) for x in filtered_mutants if len(x) == 1])
 
 
-def check_unique(normal_samples, single_mutants, all_samples):
+def check_unique(filtered_mutants, single_mutants):
     """Returns FALSE if any mutant samples were not uniquely identified"""
-    # if single_mutants merged with normals doesn't cover all samples,
-    #   at least one sample was never uniquely identified as mutant.
-    # it would be possible to simply NOT call the ambiguous sample_ids,
+    # for each normal-filtered mutant test pool,
+    #   remove any samples that were uniquely identified.
+    #   if multiple samples still remain, they cannot be uniquely identified.
+    # could report the ambiguous sample_ids and call the successful samples,
     #    but the instructions say that ALL samples must be mapped uniquely.
-    if len(normal_samples | single_mutants) != len(all_samples):
-        # return False flag for failure
-        return False
+    for test_set in filtered_mutants:
+        test_set = test_set - single_mutants
+        if len(test_set) > 1:
+            return False
     # return true if check passes
     return True
 
@@ -105,8 +133,8 @@ def check_consistent(filtered_mutants):
     # if any mutant test gets filtered to 0 elements,
     #   all of these test samples were identified in normal tests
     #   this can only happen with an erroneous NORMAL or MUTANT genotype call.
-    for test in filtered_mutants:
-        if len(test) == 0:
+    for test_set in filtered_mutants:
+        if len(test_set) == 0:
             # return False flag for failure
             return False
 
@@ -116,33 +144,28 @@ def check_consistent(filtered_mutants):
 
 def process_experiment(experiment):
     """Evaluates a single multiplexing experiment"""
-    # stores all sample ids
-    all_samples = set()
     # stores normal sample ids
     normal_samples = set()
     # stores each mutant callset
     mutant_sample_sets = []
     # for each test pool,
-    for test in experiment:
+    for test_set in experiment:
         # get genotype call from first item
-        state = test[0]
+        state = test_set[0]
         # all following items are sample_ids
-        samples = test[1:]
-        # add samples to all set
-        all_samples.update(samples)
+        samples = test_set[1:]
         # collect mutant test pools in separate sets
         if state == "MUT":
             mutant_sample_sets.append(set(samples))
         # collect all normal test ids in a single set
         elif state == "NORM":
             normal_samples.update(samples)
-
     # for each mutant test set, remove samples found in normal pools
     filtered_mutants = filter_mutants(mutant_sample_sets, normal_samples)
     # get MUT tests that were narrowed down to a single mutant
     single_mutants = get_single_mutants(filtered_mutants)
     # check for uniqueness
-    if not check_unique(normal_samples, single_mutants, all_samples):
+    if not check_unique(filtered_mutants, single_mutants):
         return False, "NONUNIQUE"
     # check for consistency
     if not check_consistent(filtered_mutants):
@@ -151,23 +174,23 @@ def process_experiment(experiment):
     return True, [single_mutants, normal_samples]
 
 
-def writer(success, result, output_file):
+def output_results(success, result, output_file):
     """Writes the results of a genotyping experiment to output"""
     # write failure state if experiment failed
     if not success:
         output_file.write(result+"\n")
     elif success:
         # unpack results sets if success
-        single_mutants, normals = result
+        single_mutants, normal_samples = result
         # list to store all calls
         final_calls = []
         # get mutant and normal counts
         n_mut = len(single_mutants)
-        n_norm = len(normals)
+        n_norm = len(normal_samples)
         # append sample_ids with genotype call to final_calls
         for sample_id in single_mutants:
             final_calls.append((sample_id, "MUT"))
-        for sample_id in normals:
+        for sample_id in normal_samples:
             final_calls.append((sample_id, "NORM"))
 
         # sort final_calls by sample_ids
@@ -184,14 +207,117 @@ def writer(success, result, output_file):
     return
 
 
-class Tester(unittest.TestCase):
+class TestArgs(unittest.TestCase):
     """Class for defining unit tests"""
+
+    def test_get_args_input(self):
+        """TEST get_args FUNCTIONALITY"""
+        # test flag
+        args, parser = get_args(["-t"])
+        self.assertTrue(args.test)
+        # only define input file
+        args, parser = get_args(["-i", "input"])
+        self.assertEqual(args.input, "input")
+        self.assertEqual(args.output, None)
+        # only define output file
+        args, parser = get_args(["-o", "output"])
+        self.assertEqual(args.input, None)
+        self.assertEqual(args.output, "output")
+        # define both
+        args, parser = get_args(["-i", "input", "-o", "output"])
+        self.assertEqual(args.input, "input")
+        self.assertEqual(args.output, "output")
+        # define none
+        args, parser = get_args([])
+        self.assertEqual(args.input, None)
+        self.assertEqual(args.output, None)
+
+class TestUtilities(unittest.TestCase):
+    """Class for defining utilities tests"""
+
+    def setUp(self):
+        """Init test module"""
+        # expected filtered mutant sets for each test
+        self.test_filtered_mutants = [
+            [],
+            [set(['12'])],
+            [set([])],
+            [set(['1', '0']), set(['1', '2'])]
+        ]
+
+        # mutant and normal sets for each test
+        self.test_sample_sets = [
+            [[], set(['1', '0', '2'])],
+            [[set(['12', '110'])], set(['100', '110'])],
+            [[set(['1', '2'])], set(['1', '0', '3', '2'])],
+            [[set(['1', '0']), set(['1', '2'])], set([])]
+        ]
+
+        #single mutant sets for each test
+        self.test_single_mutants = [set([]), set(['12']), set([]), set([])]
+        # unique check result for each test
+        self.test_uniques = [True, True, True, False]
+        # consistency check result for each test
+        self.test_consistents = [True, True, False, True]
+        # test command line args
+
+    def test_filter_mutants(self):
+        """TEST filter_mutants FUNCTIONALITY"""
+        # list to collect filtered mutants
+        filtered_mutants = []
+        # call filter_mutants on each test sample set
+        for mutant_sample_sets, normal_samples in self.test_sample_sets:
+            filtered_mutants.append(
+                # sample_set is [mutant_sample_sets, normal_samples]
+                filter_mutants(mutant_sample_sets, normal_samples)
+            )
+        # compare results to test_filtered_mutants
+        self.assertEqual(filtered_mutants, self.test_filtered_mutants)
+
+    def test_get_single_mutants(self):
+        """TEST get_single_mutants FUNCTIONALITY"""
+        # list to collect single mutants
+        single_mutants = []
+        # call get_single_mutants for
+        for filtered_mutants in self.test_filtered_mutants:
+            single_mutants.append(get_single_mutants(filtered_mutants))
+        # compare results to test_single_mutants
+        self.assertEqual(single_mutants, self.test_single_mutants)
+
+    def test_check_unique(self):
+        """TEST check_unique FUNCTIONALITY"""
+        # list to collect unique mutants
+        uniques = []
+        # zip inputs together for looping
+        test_input = zip(self.test_filtered_mutants, self.test_single_mutants)
+        # call check_unique on each test input set
+        for filtered_mutants, single_mutants in test_input:
+            uniques.append(check_unique(filtered_mutants, single_mutants))
+        # compare results to test_uniques
+        self.assertEqual(uniques, self.test_uniques)
+
+    def test_check_consistent(self):
+        """TEST check_consistent FUNCTIONALITY"""
+        # list collect consistency check output
+        consistents = []
+        # call check_consistent for each test
+        for filtered_mutants in self.test_filtered_mutants:
+            consistents.append(check_consistent(filtered_mutants))
+        # compare results to test_consistentss
+        self.assertEqual(consistents, self.test_consistents)
+
+# TEST SUITE #
+class TestGenotype(unittest.TestCase):
+    """Class for defining higher-level genotyping functions"""
+
+    # pylint: disable=too-many-instance-attributes
+    # I need to include a lot of test attributes,
+    #   as only one file is allowed for submission
 
     def setUp(self):
         """Init test module"""
         # simulate input file for testing
-        self.test_input_file = StringIO.StringIO(
-            (
+        self.test_input_file = StringIO((
             "NORM,0,1\n"
             "NORM,1,2\n"
             "NORM,0,2\n"
@@ -207,8 +333,7 @@ class Tester(unittest.TestCase):
             "MUT,0,1\n"
             "MUT,1,2\n"
             "\n"
-            )
-            )
+            ))
 
         # expected output from test input
         self.test_experiments = [
@@ -231,34 +356,82 @@ class Tester(unittest.TestCase):
                 ["MUT", "0", "1"],
                 ["MUT", "1", "2"]
             ]
-            ]
+        ]
+
+        # expected output of process_experiment for each test
+        self.test_results = [
+            (True, [set([]), set(['1', '0', '2'])]),
+            (True, [set(['12']), set(['100', '110'])]),
+            (False, 'INCONSISTENT'),
+            (False, 'NONUNIQUE')
+        ]
+
+        # expected final output for testing
+        self.test_output = (
+            "MUT COUNT: 0\n"
+            "NORM COUNT: 3\n"
+            "0,NORM\n"
+            "1,NORM\n"
+            "2,NORM\n"
+            "\n"
+            "MUT COUNT: 1\n"
+            "NORM COUNT: 2\n"
+            "12,MUT\n"
+            "100,NORM\n"
+            "110,NORM\n"
+            "\n"
+            "INCONSISTENT\n"
+            "\n"
+            "NONUNIQUE\n"
+            "\n"
+        )
 
     def test_input_parser(self):
-        """TEST PARSER FUNCTIONALITY"""
+        """TEST input_parser FUNCTIONALITY"""
         # get all experiments from simulated input
         experiments = [x for x in input_parser(self.test_input_file)]
-        # check that output matches expectation
+        # compare results to test_experiments
         self.assertEqual(experiments, self.test_experiments)
 
-    def test_evaluate(self):
-        """TEST PARSER FUNCTIONALITY"""
-        #
-        self.assertEqual(0, 0)
+    def test_process_experiment(self):
+        """TEST process_experiment FUNCTIONALITY"""
+        # collect results for each test experiment
+        results = []
+        # call process_experiments for each test
+        for experiment in self.test_experiments:
+            results.append(process_experiment(experiment))
+        # compare results to test_results
+        self.assertEqual(results, self.test_results)
 
-    def test_writer(self):
-        pass
+    def test_output_results(self):
+        """TEST output_results FUNCTIONALITY"""
+        # get StringIO simulated output file
+        output_file = StringIO()
+        # write results to StringIO
+        for success, result in self.test_results:
+            output_results(success, result, output_file)
+        # compare output to test_output
+        self.assertEqual(output_file.getvalue(), self.test_output)
 
 
 def main():
     """Main function"""
     # get command line arguments
-    args = get_args(sys.argv[1:])
+    args, parser = get_args(sys.argv[1:])
+    # if test flag, run tests
+    if args.test:
+        # with tests in same file, unittest.main() still gets argv
+        # clear argv
+        sys.argv = sys.argv[:1]
+        unittest.main()
+    # open file handles
+    set_inputs(args, parser)
     # iterate over each experiment from input file using reader
     for experiment in input_parser(args.input):
         # get success flag and results
         success, results = process_experiment(experiment)
         # write the results to output
-        writer(success, results, args.output)
+        output_results(success, results, args.output)
 
 
 if __name__ == '__main__':
